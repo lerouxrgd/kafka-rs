b4bac5100189106407add6a98c568b2dd47fdda9
Leroux Romain <leroux.rgd@gmail.com>
2019-04-30 02:15:24 +0200

    fix test

--- kafka-protocol/tests/model_test.rs
+++ kafka-protocol/tests/model_test.rs
@@ -1,11 +1,11 @@
 use kafka_protocol::codec::{decode_single, encode_single};
-use kafka_protocol::model::{create_topic_request::v0::*, CreateTopicsRequest};
+use kafka_protocol::model::{create_topics_request::v0::*, CreateTopicsRequest};
 use kafka_protocol::types::*;
 
 #[test]
 fn complex_req() {
     let val1 = CreateTopicsRequest::V0 {
-        create_topic_requests: vec![CreateTopicsRequests {
+        create_topic_requests: vec![CreateTopicRequests {
             topic: "topic".to_owned(),
             num_partitions: 32,
             replication_factor: 16,
